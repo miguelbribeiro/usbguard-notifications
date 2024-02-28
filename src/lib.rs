3114af258ae31cbceccbfee5a3cda1a430ab4ebb
Miguel Ribeiro <miguelbribeiro@tecnico.ulisboa.pt>
2024-02-28 20:43:26 +0000

    Only show a notification when it makes sense

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
+use crate::usbguard::{DeviceEvent, DeviceManager, DevicePresenceUpdate, DeviceTarget};
 use std::sync::Arc;
-use crate::usbguard::{DeviceManager, DevicePresenceUpdate};
 use tokio::sync::mpsc::Receiver;
 
 mod notifications;
@@ -9,21 +9,34 @@ mod usbguard_dbus;
 const CHANNEL_BUFFER_SIZE: usize = 64;
 
 pub async fn run() {
-    let manager = Arc::new(usbguard_dbus::DbusDeviceManager::new()
-        .await
-        .expect("should be able to connect to system bus"));
+    let device_manager = Arc::new(
+        usbguard_dbus::DbusDeviceManager::new()
+            .await
+            .expect("should be able to connect to system bus"),
+    );
 
     let notifications = notifications::Notifications::new().await.unwrap();
 
-    let mut receiver = subscribe_device_updates(manager.clone()).await;
+    let mut receiver = subscribe_device_updates(device_manager.clone()).await;
     loop {
         let update = receiver.recv().await.unwrap();
+
+        // only query user if the device was just inserted and its target is "block", otherwise
+        // ignore this device
+        match (update.event(), update.target().unwrap_or(DeviceTarget::Block)) {
+            (DeviceEvent::Insert, DeviceTarget::Block) => {},
+            _ => { continue; }
+        };
+
         let target = notifications
             .ask_target_for_device_update(&update)
             .await
             .unwrap(); // TODO don't unwrap
 
-        manager.apply_device_target(update.device_id(), target).await.unwrap();
+        device_manager
+            .apply_device_target(update.device_id(), target)
+            .await
+            .unwrap();
     }
 }
 
