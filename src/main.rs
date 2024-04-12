30d444f59d48a871faa75bba41cfd37b6304bed9
Miguel Ribeiro <miguelbribeiro@tecnico.ulisboa.pt>
2024-04-12 17:23:52 +0100

    Ensure the process is not running as root

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,26 @@
+use std::process::ExitCode;
+use libc::{geteuid, getuid, uid_t};
 use usbguard_notifications::run;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> ExitCode {
+    if is_root() {
+        eprintln!("this program should not be run as root");
+        return ExitCode::from(1);
+    }
+    
     tracing_subscriber::fmt::init();
 
     run().await;
+    unreachable!();
+}
+
+fn is_root() -> bool {
+    const UID_ROOT: uid_t = 0;
+
+    // according to the POSIX manual, both these functions shall always be successful
+    let uid_process = unsafe { getuid() };
+    let euid_process = unsafe { geteuid() };
+    
+    uid_process == UID_ROOT || euid_process == UID_ROOT
 }
