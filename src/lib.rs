4c7ca43c8aca0edbcdfab582f315748405a03afa
Miguel Ribeiro <miguelbribeiro@tecnico.ulisboa.pt>
2024-04-12 18:19:35 +0100

    Check if the USBGuard dbus service is accessible

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,12 @@
 
 use crate::ask::*;
 use crate::notifications::NotificationManager;
-use crate::usbguard::{DeviceEvent, DeviceManager, DeviceTarget, DeviceUpdate};
+use crate::usbguard::{DeviceEvent, DeviceManager, DeviceTarget, DeviceUpdate, ListDevicesFilter};
 use std::collections::HashMap;
 use std::sync::Arc;
+use anyhow::anyhow;
 use tracing::{debug, error, instrument, warn};
+use crate::usbguard::dbus::DbusDeviceManager;
 
 mod ask;
 mod notifications;
@@ -21,14 +23,7 @@ pub async fn run() {
         });
     }
 
-    let devices = usbguard::dbus::DbusDeviceManager::new().await.unwrap();
-    let devices = Arc::new(devices);
-    {
-        let devices = devices.clone();
-        tokio::spawn(async move {
-            devices.watch_device_changes().await.unwrap();
-        });
-    }
+    let devices = initialize_device_manager().await.unwrap();
 
     let mut receiver = devices.subscribe_device_changes();
     loop {
@@ -49,6 +44,26 @@ pub async fn run() {
             });
         }
     }
+}
+
+async fn initialize_device_manager() -> anyhow::Result<Arc<DbusDeviceManager>> {
+    let device_manager = DbusDeviceManager::new().await.unwrap();
+    let device_manager = Arc::new(device_manager);
+    
+    // only for checking if the dbus interface is available, the returned data isn't relevant
+    if let Err(error) = device_manager.list_devices(ListDevicesFilter::None).await {
+        return Err(anyhow!("failed to access USBGuard dbus service: {}", error));
+    }
+    
+    // initialize signal listener
+    {
+        let devices = device_manager.clone();
+        tokio::spawn(async move {
+            devices.watch_device_changes().await.unwrap();
+        });
+    }
+    
+    Ok(device_manager)
 }
 
 /// Handles a device by prompting the user and applying the new target if allowed.
